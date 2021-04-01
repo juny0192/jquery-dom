b1835238023ad6c444d235bf3e93f9742d2d4a3e
Byungjun Jung <jun@Byungjuns-MacBook-Pro.local>
2021-04-01 13:58:55 -0400

    exercise update

--- jquery-exercise.js
+++ jquery-exercise.js
@@ -32,6 +32,17 @@ $(function () {
     let direction = $(evt.target).hasClass("fa-sort-down") ? "down" : "up";
     let keyToSortBy = $(evt.target).attr("id");
     let sortedMovies = sortBy(moviesList, keyToSortBy, direction);
+
+    $("#movie-list-table").empty();
+
+    for (let movies of sortedMovies) {
+      const HTMLAppend = makeMovieDataHTML(movies);
+
+      $("#movie-list-table").append(HTMLAppend);
+    }
+
+    $(e.target).toggleClass("fa-sort-down");
+    $(e.target).toggleClass("fa-sort-up");
   });
 });
 
@@ -49,13 +60,8 @@ function makeMovieDataHTML(movieinfo) {
     `;
 }
 
-// I don't understand how this function works.
-// 1. why the array.sort(function ()) accepts two arrguments?
-// 2. I got that if arrow button's id is "rate". and
-//    I don't get this a[keyToSortBy] = +a[ketToSortBy]
-//    what the + sign for?
-function sortBy(array, keyToSortBy, direction) {
-  return array.sort(function (a, b) {
+function sortBy(arr, keyToSortBy, direction) {
+  return arr.sort(function (a, b) {
     if (keyToSortBy === "rate") {
       a[keyToSortBy] = +a[keyToSortBy];
       b[keyToSortBy] = +b[keyToSortBy];
@@ -68,3 +74,25 @@ function sortBy(array, keyToSortBy, direction) {
     return 0;
   });
 }
+
+// const arrSpl = [
+//   { title: "c movie", rate: "6", movieNum: 0 },
+//   { title: "a movie", rate: "4", movieNum: 1 },
+//   { title: "h movie", rate: "2", movieNum: 2 },
+// ];
+
+// function sorting(arr, keyToSortBy, direction) {
+//   return arr.sort(function (a, b) {
+//     if (keyToSortBy === "rate") {
+//       a[keyToSortBy] = +a[keyToSortBy];
+//       b[keyToSortBy] = +b[keyToSortBy];
+//     }
+
+//     if (a[keyToSortBy] > b[keyToSortBy]) {
+//       return direction === "up" ? 1 : -1;
+//     } else if (a[keyToSortBy] < b[keyToSortBy]) {
+//       return direction === "up" ? -1 : 1;
+//     }
+//     return 0;
+//   });
+// }
